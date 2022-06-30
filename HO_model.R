f5339d9e56bc508bed76f1bad4abb719b73aa447
Bob O'Hara <bob.ohara@ntnu.no>
2022-06-30 17:56:52 +0200

    Changed priors on B & omega

--- HO_model.R
+++ HO_model.R
@@ -11,7 +11,9 @@ varepsilon_sd <- exponential(1, dim = n_latent)
 varepsilon <- create_varepsilon(n_species, n_latent, varepsilon_sd)
 
 # Species and site predictor coefficients
-#  Give N(0,1) priors, so they are on about the same scale as the epsilons
+# omega <- create_omega(n_traits,n_latent,10)
+# B <- create_B(n_covs, n_latent, 10)
+
 omega <- t(create_betas(n_traits, n_latent, sd = 1, name = "omega"))
 B <- create_betas(n_covs, n_latent, sd = 1, name = "B")
 
@@ -19,7 +21,6 @@ B <- create_betas(n_covs, n_latent, sd = 1, name = "B")
 # LVpars <- exponential(1, dim = n_latent)
 # LVscales <- rev(cumsum(LVpars))
 
-# Infinite factor latent variable method.
 a1 <- gamma(2, 1, dim = 1)
 a2 <- gamma(2, 1, dim = 1)
 delta1 <- gamma(a1, 1, dim = 1)
@@ -27,7 +28,6 @@ delta2 <- gamma(a2, 1, dim = n_latent - 1)
 delta <- c(delta1, delta2)
 phi <- gamma(1.5, 1.5, dim = n_latent)
 tau <- phi * cumprod(delta)
-# LVscales.unsc <- 1 / tau
 LVscales <- 1 / tau
 
 # row and column scores
@@ -36,15 +36,9 @@ gamma <- omega %*% TR + varepsilon # species
 R2.z <- CalcR2(tot = z, eps = epsilon, MARGIN = 2)
 R2.gamma <- CalcR2(tot = gamma, eps = varepsilon, MARGIN = 1)
 
-
-
-
 # scale row & column scores to be mean 0, sd 1
 z.sc <- ScaleVar(z, MARGIN = 2)
 gamma.sc <- ScaleVar(gamma, MARGIN = 1)
-# z.sd <- applyVar(z, MARGIN = 2, sd=TRUE)
-# gamma.sd <- applyVar(gamma, MARGIN = 1, sd=TRUE)
-# LVscales <- LVscales.unsc*z.sd*gamma.sd
 
 thingo <-  sweep(z.sc, 2, LVscales, "*")
 eta <- sweep(thingo %*% gamma.sc, 2, int, FUN = "+")
